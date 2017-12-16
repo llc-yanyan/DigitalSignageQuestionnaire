f8bd0f56c06b60535f49e666753076adb353f843
yanai <yanaitti@gmail.com>
2017-12-16 13:00:12 +0900

    updates

---
+++ src/main/java/com/yanyan/controller/MessageController.java
@@ -0,0 +1,40 @@
+package com.yanyan.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.yanyan.form.MessageForm;
+
+@Controller
+public class MessageController {
+//    @Autowired
+//    private MessageService service;
+
+    @GetMapping("/messages")
+    public String messages(Model model) {
+        model.addAttribute("messageForm", new MessageForm());
+
+//        List<Message> messages = service.getRecentMessages(100);
+//        model.addAttribute("messages", messages);
+
+        return "messages";
+    }
+
+    @PostMapping("/messages")
+    public String messagesPost(Model model, @Valid MessageForm messageForm, BindingResult bindingResult, HttpServletRequest request) {
+        if (bindingResult.hasErrors()) {
+//            List<Message> messages = service.getRecentMessages(100);
+//            model.addAttribute("messages", messages);
+            return "messages";
+        }
+
+//        service.save(new Message(messageForm.getName(), messageForm.getText(), request.getRemoteAddr()));
+        return "redirect:/messages";
+    }
+}
