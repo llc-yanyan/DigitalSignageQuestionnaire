cb34fa0c20f50ab63007c1fb20971e143879265a
yanai <yanaitti@gmail.com>
2017-12-16 12:51:08 +0900

    first commit

---
+++ src/main/java/com/yanyan/DigitalSignageQuestionnaireApplication.java
@@ -0,0 +1,12 @@
+package com.yanyan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalSignageQuestionnaireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalSignageQuestionnaireApplication.class, args);
+	}
+}
